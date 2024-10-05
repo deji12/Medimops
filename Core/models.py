ff74c269ae5045b24cdcdb143336766f20dcd582
deji12 <adesolaayodeji53@gmail.com>
2024-10-05 16:04:57 +0100

    Bot check for specified products prices and compares with max_price defined in model before adding to cart

--- Core/models.py
+++ Core/models.py
@@ -15,3 +15,9 @@ class BotControl(models.Model):
 
     medimops_account_email = models.EmailField(max_length=255, null=True, blank=True)
     medimops_account_password = models.CharField(max_length=255, null=True, blank=True)
+
+class ProductMaxPrice(models.Model):
+    item_name = models.CharField(max_length=255)
+    max_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
